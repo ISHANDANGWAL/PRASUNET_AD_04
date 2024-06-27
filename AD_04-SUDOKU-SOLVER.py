74ff6f7078da6e2b03094a587a1b9cd4b92d2901
ISHANDANGWAL <ishan.dangwal@gmail.com>
2024-06-27 17:49:54 +0530

    Add files via upload

---
+++ AD_04-SUDOKU-SOLVER.py
@@ -0,0 +1,72 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) if num != 0 else '.' for num in row))
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # Check the row
+    if any(board[row][i] == num for i in range(9)):
+        return False
+
+    # Check the column
+    if any(board[i][col] == num for i in range(9)):
+        return False
+
+    # Check the 3x3 grid
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True
+
+    row, col = empty
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+def main():
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Unsolved Sudoku Board:")
+    print_board(board)
+
+    if solve_sudoku(board):
+        print("\nSolved Sudoku Board:")
+        print_board(board)
+    else:
+        print("No solution exists.")
+
+if __name__ == "__main__":
+    main()
